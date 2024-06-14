47210ec8bd7c52d092562b08841df0e048c194ca
AbdelrahmanElnagaar <abdelrahmanelnagaar362@gmail.com>
2024-06-14 15:24:36 +0200

    add session 2

--- session1/abdelrahman_ngar.php
+++ session1/abdelrahman_ngar.php
@@ -1,3 +1,34 @@
+<?php
+
+//echo phpversion();
+//echo  "<h1>ngaar</h1>" , "<span>hello</span>";
+//echo ' ngaar' . 'this is for concat';
+//print "<h1>nagaar from print </h1>";
+//echo 'ahmed\'s car' ;
+//echo "<br>";
+
+//$myname= "ali20";
+//$var2="33";
+//echo "$myname dssdbsbs $var2";
+//echo "<br>";
+//echo '$myname dssdbsbs' . $myname;
+//echo "<br>";
+
+//$x=90;
+//$y=&$x;
+//$x=200;
+//echo $y;
+
+// $x='name';
+// $$x="7454" ;
+// echo "my name is {$$x}";
+// var_dump($x);
+
+$isConnected=true;
+$isOffline=false;
+var_dump($isConnected);
+var_dump($isOffline);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
---
+++ session2/php foundamental.php
@@ -0,0 +1,97 @@
+<?php
+// echo PHPVERSION();
+// echo "<br>";
+// echo 'hello' ,  " echo accept more than parameter and print dont" . " this is for concat" . ' Ahmed\'s car' ;
+// print "<br>this print method and return int</b>";
+
+//diffrence between single and double
+
+// $ahmed20='ali' . $var3='25';
+// echo "my name is $ahmed20 and my age is  $var3";
+// echo '<br>';
+// echo ' my name is ' . $ahmed20 . 'and mt age is ' . $var3;
+
+// echo "$ahmed20 fpdbdb"; 
+// echo ' $ahmed20 fpdbdb' ;
+
+// $firstName='ali';
+// $lastName='ahmed';
+// echo $firstName;
+// echo '$firstName';
+// echo "$firstName";
+// echo "my name is $firstName $lastName";
+// echo 'my name is $firstName $lastName';
+// echo "my name is {$firstName} {$lastName}";
+// echo 'my name is {$firstName} {$lastName}';
+
+
+//varibles , cs , loosly_typed_language , assigned_by_value-by_default , 
+
+// $myNameIs= 'ali';
+// $MyNameIs='ali30';
+
+/* $x='9';
+$y= &$x; //assigned by refrence (&)
+$x='800';
+echo "$y";
+echo " $x"; */
+
+// comment // */ */  /** */
+
+//constant defined and have value is the same line global
+// define("DATABASENAME",'ali');
+// function test(){
+//     echo DATABASENAME;
+// }
+// test() ;
+
+//predefined constant
+// echo PHP_OS_FAMILY;
+// echo PHP_VERSION;
+
+//magic constant 9msgic 7 start with __ (dir class traitt namespace) file line  function 
+// echo __DIR__ ;
+// echo __LINE__ ;
+
+// variable variables
+// $x="name";
+// $$x= 'fathy';
+// echo $name;
+// echo " my name is $name";
+// echo "my name is {$$x}";
+
+//soft debug
+//var_dump($x);
+//print_r($$x); array
+
+//data types :- 
+//scalar types  bool(false=empty string , true=1) int float string
+//computed types  array object callable iterable
+//speacial types resource null
+
+//trothy values , falsey values= false,0,-0,0.0,-0.0,null,[],'0',''
+// $test = "0";
+// if($test==true){
+//     echo 'this is truthy value';
+// }else{
+//     echo 'this is falsy value';
+// }
+
+//cont predefined constant
+// echo PHP_INT_MAX;   //large systems
+// echo PHP_INT_MIN;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <h1><?php echo  "$firstName"; ?></h1>
+    <h1><?=  "$lastName" ?></h1>  -->
+</body>
+</html>
